8109d3ab66c67e1b012e990220e152ade0aa8d64
AnahiGuanotasig <guanotasig.anahi1n9@gmail.com>
2026-05-13 23:47:58 -0500

    Conexion de postgresql
    Conexion de postgres a node, y creacion del archivo .env para las credenciales de la base de datos.

---
+++ src/config/databaje.js
@@ -0,0 +1,24 @@
+import PG from "pg";
+import dotenv from "dotenv";
+
+//lector de las variables de entorno
+dotenv.config();
+
+//Pool de conexiones
+export const pool = new PG.Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+//Compuebo la conexion
+pool.connect()
+    .then(client => {
+        console.log("Conectado a PostgreSQL");
+        client.release();
+    })
+    .catch(err => {
+        console.error("Error conectando a PostgreSQL:", err.message);
+    });
